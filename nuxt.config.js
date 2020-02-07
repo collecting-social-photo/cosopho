46102f4891201d5883c19fe315b013f891a2de75
Giv Parvaneh <giv@givp.org>
2020-02-07 10:20:02 -0800

    Updating favicon

--- nuxt.config.js
+++ nuxt.config.js
@@ -19,7 +19,7 @@ module.exports = {
       { hid: 'description', name: 'description', content: process.env.npm_package_description || '' }
     ],
     link: [
-      { rel: 'icon', type: 'image/x-icon', href: '/favicon.ico' },
+      { rel: 'icon', type: 'image/x-icon', href: '/favicon.png' },
       { rel: 'stylesheet', href: 'https://fonts.googleapis.com/css?family=Roboto:400,500,700,400italic|Material+Icons' }
     ],
     script: [
