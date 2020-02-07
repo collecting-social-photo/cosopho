09f277691f56af2787846473198d0b427db0889b
Giv Parvaneh <giv@givp.org>
2020-02-07 10:10:35 -0800

    Handling no photos

--- pages/explore/index.vue
+++ pages/explore/index.vue
@@ -1,20 +1,25 @@
 <template>
   <div class="container">
     <h1 :style="`color:#${$store.state.instance.colour};`">Explore</h1>
-    <div class="photo-list" v-for="photo in photos" :key="photo.id">
-      <nuxt-link :to="localePath({ name: 'explore-photo-id', params: { id: photo.id }})">
-        <img :src="`https://res.cloudinary.com/hftpxlihv/image/upload/w_1000/v1576673295/${photo.data.public_id}.jpg`" />
-      </nuxt-link>
-    </div>
+    <div v-if="photos.length">
+      <div class="photo-list" v-for="photo in photos" :key="photo.id">
+        <nuxt-link :to="localePath({ name: 'explore-photo-id', params: { id: photo.id }})">
+          <img :src="`https://res.cloudinary.com/hftpxlihv/image/upload/w_1000/v1576673295/${photo.data.public_id}.jpg`" />
+        </nuxt-link>
+      </div>
 
-    <div class="spinner objects-spinner" v-bind:class="spinnerClass">
-      <div class="dot-flashing"></div>
-    </div>
+      <div class="spinner objects-spinner" v-bind:class="spinnerClass">
+        <div class="dot-flashing"></div>
+      </div>
 
-    <div v-if="page < maxPage" v-observe-visibility="{
-      callback: visibilityChanged,
-      once: false,
-    }"></div>
+      <div v-if="page < maxPage" v-observe-visibility="{
+        callback: visibilityChanged,
+        once: false,
+      }"></div>
+    </div>
+    <div v-else>
+      No photos found 😥
+    </div>
   </div>
 </template>
 
@@ -30,12 +35,15 @@ export default {
     }
   },
   async asyncData (context) {
+    var maxPage = 1
     let response = await context.app.$api.getPhotos({
       instance: context.app.store.state.instance.id,
       per_page: 2
     })
     const photos = response.data.data.photos
-    const maxPage = photos[0]._sys.pagination.maxPage
+    if (photos.length) {
+      maxPage = photos[0]._sys.pagination.maxPage
+    }
 
     return { photos: photos, maxPage: maxPage }
   },
