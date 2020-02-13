7cb8306402d8564b109872230fa2ca48f0ff1fd5
Giv Parvaneh <giv@givp.org>
2020-02-12 21:33:37 -0800

    File upload wip

--- pages/contribute/_id.vue
+++ pages/contribute/_id.vue
@@ -1,15 +1,27 @@
 <template>
   <div class="container">
     Upload to {{ selectedInitiative }} here...
+    <div>
+      <dropzone id="foo" ref="el" :options="options" :destroyDropzone="true"></dropzone>
+    </div>
   </div>
 </template>
 
 <script>
+import Dropzone from 'nuxt-dropzone'
+import 'nuxt-dropzone/dropzone.css'
+
 export default {
   middleware: 'auth',
+  components: {
+    Dropzone
+  },
   data () {
     return {
-      selectedInitiative: null
+      selectedInitiative: null,
+      options: {
+        url: "http://httpbin.org/anything"
+      }
     }
   },
   async asyncData (context) {
@@ -17,6 +29,10 @@ export default {
       return context.redirect(context.app.localePath({ name: 'contribute' }))
     }
     return { selectedInitiative: context.params.id }
+  },
+  mounted() {
+    // Everything is mounted and you can access the dropzone instance
+    const instance = this.$refs.el.dropzone
   }
 }
 </script>
