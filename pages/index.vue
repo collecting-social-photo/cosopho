df8c07281a5255e17f452cb948288c853170c930
Giv Parvaneh <giv@givp.org>
2020-02-27 21:18:40 -0800

    Fixed homepage

--- pages/index.vue
+++ pages/index.vue
@@ -1,31 +1,44 @@
 <template>
-  <div class="container">
-    <div>
-      <div><img :src="`data:image/png;base64,${$store.state.instance.logo}`" class="logo" width="100"></div>
-      <div>
-        <md-button :to="localePath('explore')" class="md-primary md-raised">
-          Explore
-        </md-button>
-        <md-button :to="localePath('contribute')" class="md-primary  md-raised">
-          Contribute
-        </md-button>
+  <div>
+    <div :style="`background-image: url(https://res.cloudinary.com/hftpxlihv/image/upload/w_1000/v1576673295/${photo.data.public_id}.jpg);`" class="intro">
+      <div class="inner">
+        <div class="container">
+          <div class="intro-logo">
+            <img :src="`data:image/png;base64,${$store.state.instance.logo}`">
+          </div>
+          <div class="intro-text">
+            Collecting Social Photographs
+          </div>
+          <div class="intro-about">
+            Donate your digital photos to the museum. <nuxt-link :to="localePath('about')">Read more</nuxt-link>.
+          </div>
+          <div class="intro-buttons">
+            <md-button :to="localePath('contribute')" class="md-secondary button-big">
+              Contribute
+            </md-button>
+            <md-button :to="localePath('explore')" class="md-secondary button-big">
+              Explore
+            </md-button>
+          </div>
+        </div>
       </div>
-      <photoComp v-if="photo" :publicId="photo.data && photo.data.public_id" />
     </div>
-    <h1>Featured initiatives</h1>
-    <p>Explore photos within current collecting initiatives.</p>
+    <div class="container">
+      <h1>Featured initiatives</h1>
+      <p>Explore photos within current collecting initiatives.</p>
 
-    <div v-for="initiative in initiatives" :key="initiative.slug">
-      <h2>
-        <nuxt-link :to="localePath({ name: 'initiatives-id', params: { id: initiative.slug }})">
-          {{ initiative.title }}
-        </nuxt-link>
-      </h2>
-      <div class="md-layout md-gutter md-alignment-center">
-        <div v-for="photo in initiative.photos.slice(0,4)" :key="photo.data.public_id" class="md-layout-item">
-          <nuxt-link :to="localePath({ name: 'explore-photo-id', params: { id: photo.id }})">
-            <photoComp v-if="photo" :publicId="photo.data && photo.data.public_id" />
+      <div v-for="initiative in initiatives" :key="initiative.slug">
+        <h2>
+          <nuxt-link :to="localePath({ name: 'initiatives-id', params: { id: initiative.slug }})">
+            {{ initiative.title }}
           </nuxt-link>
+        </h2>
+        <div class="md-layout md-gutter md-alignment-center">
+          <div v-for="photo in initiative.photos.slice(0,4)" :key="photo.data.public_id" class="md-layout-item">
+            <nuxt-link :to="localePath({ name: 'explore-photo-id', params: { id: photo.id }})">
+              <photoComp v-if="photo" :publicId="photo.data && photo.data.public_id" />
+            </nuxt-link>
+          </div>
         </div>
       </div>
     </div>
